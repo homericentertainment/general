8d7f476da2cafdba337f2a25e2697f01ca691e10
idanberger22 <idanberger22@gmail.com>
2025-10-20 16:24:48 +0300

    redirects

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
-import { Suspense, lazy } from 'react'
 
 // pages
 import { ErrorPage } from './pages/error.jsx'
-const Home = lazy(() => import('./pages/home.jsx'))
-const TreasureHunt = lazy(() => import('./pages/treasure-hunt.jsx'))
+import Home from './pages/home.jsx'
+import TreasureHunt from './pages/treasure-hunt.jsx'
 
 // components
 import { Footer } from './components/footer.jsx'
-import { Loader } from './components/loader.jsx'
 
 function App() {
 
@@ -17,19 +15,14 @@ function App() {
       path: '/',
       element: <section>
         <div className='main'>
-          <Suspense fallback={<div className='screen-center'>
-            <Loader />
-          </div> }>
             <Outlet />
-          </Suspense>
         </div>
         <Footer />
       </section>,
       errorElement: <ErrorPage />,
       children: [
-        { index: true, element: <Home /> },
-        { path: '/', element: <Home /> },
         { path: 'treasure-hunt', element: <TreasureHunt /> },
+        { path: '/', element: <Home /> },
       ],
     },
   ])
